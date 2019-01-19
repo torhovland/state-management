667dcc7dc4fa3731ece0d8576db22af73fc0a146
Tor Hovland <tor.hovland@bekk.no>
2019-01-19 19:19:15 +0100

    Completing todos and counting todos.

--- vanillajs/main.js
+++ vanillajs/main.js
@@ -1,11 +1,40 @@
 function addTodo()
 {
-    const todos = document.getElementById('todos');
     const newTodo = document.getElementById('newTodo');
     const text = newTodo.value;
     const textNode = document.createTextNode(text);
-    var todoNode = document.createElement("li");
+
+    const checkNode = document.createElement("input");
+    checkNode.setAttribute("type", "checkbox");
+    checkNode.onclick = completeTodo;
+
+    const todoNode = document.createElement("div");
+    todoNode.appendChild(checkNode);   
     todoNode.appendChild(textNode);   
+    
+    const todos = document.getElementById('todos');
     todos.appendChild(todoNode);
+
     newTodo.value = "";
+
+    updateStatistics();
 }
+
+function completeTodo(e)
+{
+    const todoNode = e.path[1];
+    todoNode.remove();
+
+    updateStatistics();
+}
+
+function updateStatistics()
+{
+    const todos = document.getElementById('todos');
+    const children = todos.children;
+
+    const countNode = document.getElementById('numberOfTodos');
+    countNode.innerText = children.length;
+}
+
+updateStatistics();
